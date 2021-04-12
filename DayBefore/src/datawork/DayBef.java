f7ed5a871581915c7207ac2c3f58149584a29d28
Zinovij Fedorenko <zfedorenko@mail.ru>
2021-04-12 16:24:32 +0300

    DayNextBirth

---
+++ DayBefore/src/datawork/DayBef.java
@@ -0,0 +1,47 @@
+package datawork;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Scanner;
+
+public class DayBef {
+
+	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+		LocalDate localDate = LocalDate.now();
+		System.out.println("************* Введите дату рождения *************************");
+		System.out.println("Год: ");
+        int bYear = in.nextInt();
+        System.out.println("Месяц: ");
+        int bMon = in.nextInt();
+        System.out.print("День: ");
+        int bDay = in.nextInt();
+        in.close();
+        
+        LocalDate birthDateInNowYear=LocalDate.of(localDate.getYear(), bMon, bDay);
+		LocalDate newbirthDate = LocalDate.of(1,1,1);
+		if (birthDateInNowYear.isAfter(localDate)) {
+			newbirthDate = LocalDate.of(localDate.getYear(), bMon, bDay);
+		}
+        if (birthDateInNowYear.isBefore(localDate)) {
+        	newbirthDate = LocalDate.of(localDate.getYear()+1, bMon, bDay);
+		}
+         if (birthDateInNowYear.isEqual(localDate)) {
+        	 newbirthDate = LocalDate.of(localDate.getYear()+1, bMon, bDay);
+		}
+ 
+        Date dat1= new Date();
+        Date dat2= new Date();
+        
+        dat1=Date.from(newbirthDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        dat2=Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        
+        
+        System.out.println("************* Количество дней до дня рождения ***************");
+        System.out.println( (dat1.getTime()-dat2.getTime())/(24*60*60*1000)  );
+	}
+	
+
+}
